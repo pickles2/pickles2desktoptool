2d58e325ff555f39d94e07516936102887bd3722
Tomoya Koyanagi <tomk79@gmail.com>
2014-12-10 23:15:56 +0900

    jQuery SVG: 断念
    - ちょいちょいいろいろやってみたが、svg要素の高さを変えられない、というところで躓いて断念。
    - div要素で普通にやる方針に切り替えようと思う。
    - シェイプが必要なときは、各divの中にsvgを作ることにする。よく考えたらそれで十分。

--- app/mods/editor/editor_gui_files/app.ui.js
+++ app/mods/editor/editor_gui_files/app.ui.js
@@ -15,7 +15,7 @@ window.contApp.ui = new(function(px, contApp){
 		$preview = $('iframe.cont_field-preview');
 		$previewDoc = $($preview[0].contentWindow.document);
 		$ctrlPanel = $('.cont_field-ctrlpanel');
-		$ctrlPanelSvg = $('<svg>');
+		$ctrlPanel.svg();
 		$palette = $('.cont_modulelist');
 
 		// モジュールパレットの初期化
@@ -49,22 +49,23 @@ window.contApp.ui = new(function(px, contApp){
 		;
 
 		// 編集フィールドの初期化
-		$ctrlPanel
-			.html('')
-			.append( $ctrlPanelSvg
-				// .bind('drop', function(e){
-				// 	var modId = event.dataTransfer.getData("moduleId");
-				// 	px.message( 'modId "'+modId+'" がドロップされました。' );
-				// })
-				// .bind('dragover', function(e){
-				// 	event.preventDefault();
-				// 	// px.message(456);
-				// })
-				// .bind('click', function(e){
-				// 	px.message('TEST: Clicked');
-				// })
-			)
-		;
+		// $ctrlPanel
+		// 	.html('')
+		// 	.append( $ctrlPanelSvg
+		// 		// .bind('drop', function(e){
+		// 		// 	var modId = event.dataTransfer.getData("moduleId");
+		// 		// 	px.message( 'modId "'+modId+'" がドロップされました。' );
+		// 		// })
+		// 		// .bind('dragover', function(e){
+		// 		// 	event.preventDefault();
+		// 		// 	// px.message(456);
+		// 		// })
+		// 		// .bind('click', function(e){
+		// 		// 	px.message('TEST: Clicked');
+		// 		// })
+		// 	)
+		// ;
+		// $ctrlPanelSvg.clear();
 
 		$preview
 			.bind('load', function(){
@@ -117,37 +118,56 @@ window.contApp.ui = new(function(px, contApp){
 
 		this.$elmParent.append(this.$elm);
 
-		this.$svgElm = $('<rect>')
-			.css({
-				'border':'3px dotted #99d',
-				'text-align':'center',
-				'background-color': '#ddf',
-				'display':'block',
-				'position':'absolute'
-			})
-			.attr({
-				'x':this.$elm.offset().left ,
-				'y':this.$elm.offset().top ,
-				'width': this.$elm.width(),
-				'height': this.$elm.height()
-			})
-			.width(this.$elm.width())
-			.height(this.$elm.height())
-			.offset(this.$elm.offset())
-			.text('ここにモジュールをドラッグしてください。')
-			.bind('drop', function(e){
-				var modId = event.dataTransfer.getData("moduleId");
-				px.message( 'modId "'+modId+'" がドロップされました。' );
-			})
-			.bind('dragover', function(e){
-				event.preventDefault();
-				// px.message(456);
-			})
-			.bind('click', function(e){
-				px.message('TEST: Clicked');
-			})
-		;
-		$ctrlPanelSvg.append( this.$svgElm );
+		// this.$svgElm = $('<rect>')
+		// 	.css({
+		// 		'border':'3px dotted #99d',
+		// 		'text-align':'center',
+		// 		'background-color': '#ddf',
+		// 		'display':'block',
+		// 		'position':'absolute'
+		// 	})
+		// 	.attr({
+		// 		'x':this.$elm.offset().left ,
+		// 		'y':this.$elm.offset().top ,
+		// 		'width': this.$elm.width(),
+		// 		'height': this.$elm.height()
+		// 	})
+		// 	.width(this.$elm.width())
+		// 	.height(this.$elm.height())
+		// 	.offset(this.$elm.offset())
+		// 	.text('ここにモジュールをドラッグしてください。')
+		// 	.bind('drop', function(e){
+		// 		var modId = event.dataTransfer.getData("moduleId");
+		// 		px.message( 'modId "'+modId+'" がドロップされました。' );
+		// 	})
+		// 	.bind('dragover', function(e){
+		// 		event.preventDefault();
+		// 		// px.message(456);
+		// 	})
+		// 	.bind('click', function(e){
+		// 		px.message('TEST: Clicked');
+		// 	})
+		// ;
+		// $ctrlPanelSvg.append( this.$svgElm );
+
+		this.$svgElm = $ctrlPanelSvg.rect(
+			this.$elm.offset().left, this.$elm.offset().top, this.$elm.width(), this.$elm.height(),
+			{
+				fill: '#ddf', stroke: '#99d', strokeWidth: 3
+			}
+		);
+		this.$svgElm.ondragover = function(event){
+			event.preventDefault();
+			// px.message(456);
+		};
+		this.$svgElm.ondrop = function(event){
+			var modId = event.dataTransfer.getData("moduleId");
+			px.message( 'modId "'+modId+'" がドロップされました。' );
+		};
+		this.$svgElm.onclick = function(event){
+			px.message('TEST: Clicked');
+		};
+		// console.log( this.$svgElm );
 	}
 
 	/**
@@ -164,13 +184,23 @@ window.contApp.ui = new(function(px, contApp){
 		var fieldheight = $previewDoc.find('body').height()+5;
 		$preview.height( fieldheight );
 		$ctrlPanel.height( fieldheight );
-		$ctrlPanelSvg
-			.css({'display':'block'})
-			.height( $ctrlPanel.height() )
-			.width( $ctrlPanel.width() )
-		;
 
-		$ctrlPanelSvg.html('');
+		$ctrlPanel.svg();
+		$ctrlPanelSvg = $ctrlPanel.svg('get');
+		$ctrlPanelSvg
+			// .css({'display':'block'})
+			.clear()
+			// .setAttribute({
+			// 	"height": $ctrlPanel.height(),
+			// 	"width": $ctrlPanel.width()
+			// })
+			// .height( $ctrlPanel.height() )
+			// .width( $ctrlPanel.width() )
+			// }
+		;
+		// $ctrlPanelSvg.attributes.height = $ctrlPanel.height();
+
+		// $ctrlPanelSvg.clear();
 		$previewDoc.find('.contents').each(function(){
 			$(this).html('');
 			var id = $(this).attr('id')||'main';

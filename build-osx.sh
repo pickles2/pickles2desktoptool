caa274002848414884cd2b0267fb756d77fff327
Tomoya Koyanagi <tomk79@gmail.com>
2015-01-07 13:07:40 +0900

    OSX用アプリケーションアイコン追加
    - IconComposerでアイコンを作れなかったのは、ソースが 8bit PNG だったからみたい。24bit PNG にしたらできた。

--- build-osx.sh
+++ build-osx.sh
@@ -1,6 +1,7 @@
 appname=dist/osx/Pickles2DesktopTool
 rm -r ${appname}.app
 mkdir ${appname}.app
+cp ./app/common/images/px2-osx.icns node_modules/nodewebkit/nodewebkit/node-webkit.app/Contents/Resources/nw.icns
 cp -r node_modules/nodewebkit/nodewebkit/node-webkit.app/Contents ${appname}.app
 zip -r ${appname}.app/Contents/Resources/app.nw ./app ./package.json ./node_modules
 #open ./${appname}.app

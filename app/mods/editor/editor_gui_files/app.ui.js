5e9a1e9c04b585cde2f3dd81a70e2f802a0f4d6d
Tomoya Koyanagi <tomk79@gmail.com>
2015-01-12 20:01:17 +0900

    GUIエディタで、png以外の画像タイプを扱えるようになった。

--- app/mods/editor/editor_gui_files/app.ui.js
+++ app/mods/editor/editor_gui_files/app.ui.js
@@ -549,9 +549,9 @@ window.contApp.ui = new(function(px, contApp){
 					var field = modTpl.fields[idx];
 					if( field.fieldType == 'input' ){
 						if( contApp.fieldDefinitions[field.type] ){
-							data.fields[field.name] = contApp.fieldDefinitions[field.type].saveEditorContent( $editWindow.find('form [data-field-unit='+JSON.stringify( modTpl.fields[idx].name )+']') );
+							data.fields[field.name] = contApp.fieldDefinitions[field.type].saveEditorContent( $editWindow.find('form [data-field-unit='+JSON.stringify( modTpl.fields[idx].name )+']'), data.fields[field.name] );
 						}else{
-							data.fields[field.name] = contApp.fieldBase.saveEditorContent( $editWindow.find('form [data-field-unit='+JSON.stringify( modTpl.fields[idx].name )+']') );
+							data.fields[field.name] = contApp.fieldBase.saveEditorContent( $editWindow.find('form [data-field-unit='+JSON.stringify( modTpl.fields[idx].name )+']'), data.fields[field.name] );
 						}
 					}else if( field.fieldType == 'module' ){
 						// module: 特に処理なし

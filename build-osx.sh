285746d061a3fa7e21702f1ee7d6137a7885df01
Tomoya Koyanagi <tomk79@gmail.com>
2015-01-14 22:04:22 +0900

    Info.plist を編集、他
    - ビルド済みのZIPは消すことにした。重いし。

--- build-osx.sh
+++ build-osx.sh
@@ -1,12 +1,8 @@
 appname=dist/osx/Pickles2DesktopTool
 rm -r ${appname}.app
 mkdir ${appname}.app
-# node_modules/nodewebkit/nodewebkit/node-webkit.app/Contents/Info.plist
-# CFBundleDisplayName -> Pickles 2 Desktop Tool
-# CFBundleName -> Pickles 2 Desktop Tool
-# CFBundleShortVersionString -> (version number)
-# CFBundleVersion -> (build number?)
-cp ./app/common/images/px2-osx.icns node_modules/nodewebkit/nodewebkit/node-webkit.app/Contents/Resources/nw.icns
+cp ./app/common/build/Info.plist node_modules/nodewebkit/nodewebkit/node-webkit.app/Contents/Info.plist
+cp ./app/common/build/px2-osx.icns node_modules/nodewebkit/nodewebkit/node-webkit.app/Contents/Resources/nw.icns
 cp -r node_modules/nodewebkit/nodewebkit/node-webkit.app/Contents ${appname}.app
 zip -r ${appname}.app/Contents/Resources/app.nw ./app ./package.json ./node_modules
 #open ./${appname}.app

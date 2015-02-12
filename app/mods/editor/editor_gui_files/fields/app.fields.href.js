e3d366e68180ff05daa407b9f8297eb9ee6fd859
Tomoya Koyanagi <tomk79@gmail.com>
2015-02-12 11:34:29 +0900

    hrefフィールド追加

---
+++ app/mods/editor/editor_gui_files/fields/app.fields.href.js
@@ -0,0 +1,29 @@
+window.contApp.fieldDefinitions.href = _.defaults( new (function( px, contApp ){
+
+	/**
+	 * データをバインドする
+	 */
+	this.bind = function( fieldData, mode ){
+		var rtn = ''
+		if(typeof(fieldData)===typeof('')){
+			rtn = px.$('<div>').text( fieldData ).html(); // ←HTML特殊文字変換
+			// rtn = rtn.replace(new RegExp('\r\n|\r|\n','g'), '<br />'); // ← 属性値などに使うので、改行コードは改行コードのままじゃないとマズイ。
+		}
+		if( mode == 'canvas' && !rtn.length ){
+			// rtn = $('<span>')
+			// 	.text('(ダブルクリックしてテキストを編集してください)')
+			// 	.css({
+			// 		'color':'#999',
+			// 		'background-color':'#ddd',
+			// 		'font-size':'10px',
+			// 		'padding':'0 1em'
+			// 	})
+			// 	.get(0).outerHTML
+			// ;
+			// ↑属性値などに使うので、HTMLタグを含むのはマズイ。
+			rtn = '';
+		}
+		return rtn;
+	}
+
+})( window.px, window.contApp ), window.contApp.fieldBase );

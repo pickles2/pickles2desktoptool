c3533729fb759ee77946b9925e161353c839d261
Tomoya Koyanagi <tomk79@gmail.com>
2015-02-14 12:24:09 +0900

    hrefフィールドを、ページ一覧から選択できるようにした。

--- app/mods/editor/editor_gui_files/fields/app.fields.href.js
+++ app/mods/editor/editor_gui_files/fields/app.fields.href.js
@@ -26,4 +26,99 @@ window.contApp.fieldDefinitions.href = _.defaults( new (function( px, contApp ){
 		return rtn;
 	}
 
+	/**
+	 * エディタUIを生成
+	 */
+	this.mkEditor = function( mod, data ){
+		var changeTimer;
+		var blurTimer;
+		function onChange(){
+			clearTimeout(changeTimer);
+			var $this = $(this);
+			changeTimer = setTimeout(function(){
+				var pages = $this.data('pages');
+				var $html = $('<ul>');
+				for( var idx in pages ){
+					if( !pages[idx].path.match( new RegExp('^'+px.utils.escapeRegExp($this.val())) ) ){
+						continue;
+					}
+					$html
+						.append( $('<li>')
+							.append( $('<a>')
+								.css({
+									'display':'block'
+								})
+								.attr({
+									'href': 'javascript:;',
+									'data-path': pages[idx].path
+								})
+								.text( pages[idx].path +' ('+pages[idx].title+')' )
+								.click(function(){
+									$input
+										.val( $(this).attr('data-path') )
+										.focus()
+									;
+								})
+							)
+						)
+					;
+				}
+				$palatte.html('').append( $html );
+			}, 100);
+		}
+		var $input = $('<input>')
+			.attr({
+				"name":mod.name
+			})
+			.val(data)
+			.data( 'pages', px.getCurrentProject().site.getSitemap() )
+			.css({'width':'100%','height':'auto'})
+			.change( onChange )
+			.keyup( onChange )
+			.focus(function(){
+				clearTimeout( blurTimer );
+				$palatte.show();
+			})
+			.blur(function(){
+				clearTimeout( blurTimer );
+				blurTimer = setTimeout( function(){
+					$palatte.hide('slow');
+				}, 500 );
+			})
+			.change()
+		;
+		var $palatte = $('<div>')
+			.css({
+				'height':200,
+				'overflow':'auto',
+				'position':'absolute',
+				'background':'#f9f9f9',
+				'opacity':'0.9',
+				'width':'100%'
+			})
+			.hide()
+		;
+		return $('<div>')
+			.append( $input )
+			.append( $('<div>')
+				.css({
+					'position':'relative'
+				})
+				.click(function(){
+					clearTimeout( blurTimer );
+				})
+				.append( $palatte )
+			)
+		;
+	}
+
+	/**
+	 * エディタUIで編集した内容を保存
+	 */
+	this.saveEditorContent = function( $dom, data ){
+		var src = $dom.find('input').val();
+		src = JSON.parse( JSON.stringify(src) );
+		return src;
+	}
+
 })( window.px, window.contApp ), window.contApp.fieldBase );

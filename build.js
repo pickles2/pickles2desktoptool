14a805ac9dc8748bfa1b5da3abb3ee1fb9dda19e
Tomoya Koyanagi <tomk79@gmail.com>
2015-05-26 16:48:14 +0900

    build後にZIPに固めるようにした。

--- build.js
+++ build.js
@@ -1,5 +1,23 @@
-console.log('Build...');
+var _utils = require('./app/index_files/_utils.node.js');
 var NwBuilder = require('node-webkit-builder');
+var zipFolder = require('zip-folder');
+
+console.log('== build "Pickles 2 Desktop Tool" ==');
+
+console.log('Cleanup...');
+if( _utils.isDirectory(__dirname+'/build/pickles2desktoptool/') ){
+  _utils.rmdir_r(__dirname+'/build/pickles2desktoptool/');
+}
+if( _utils.isFile(__dirname+'/build/Pickles2DesktopTool-osx32.zip') ){
+  _utils.rm(__dirname+'/build/Pickles2DesktopTool-osx32.zip');
+}
+if( _utils.isFile(__dirname+'/build/Pickles2DesktopTool-win32.zip') ){
+  _utils.rm(__dirname+'/build/Pickles2DesktopTool-win32.zip');
+}
+console.log('');
+
+
+console.log('Build...');
 var nw = new NwBuilder({
     files: [
       './package.json',
@@ -14,19 +32,60 @@ var nw = new NwBuilder({
     ]
 });
 
-/*
-cp ./app/common/build/Info.plist node_modules/nodewebkit/nodewebkit/node-webkit.app/Contents/Info.plist
-cp ./app/common/build/px2-osx.icns node_modules/nodewebkit/nodewebkit/node-webkit.app/Contents/Resources/nw.icns
-cp -r node_modules/nodewebkit/nodewebkit/node-webkit.app/Contents ${appname}.app
-zip -r ${appname}.app/Contents/Resources/app.nw ./app ./package.json ./node_modules
-*/
-
 //Log stuff you want
 nw.on('log',  console.log);
 
 // Build returns a promise
 nw.build().then(function () {
-  console.log('all done!');
+
+  console.log('all build done!');
+
+  (function(){
+
+    _utils.iterateFnc([
+      function(itPj, param){
+        console.log('ZIP mac32...');
+        zipFolder(
+          __dirname + '/build/pickles2desktoptool/osx32/',
+          __dirname + '/build/Pickles2DesktopTool-osx32.zip',
+          function(err) {
+            if(err) {
+                console.log('ERROR!', err);
+            } else {
+                console.log('success.');
+            }
+            itPj.next();
+        });
+      },
+      function(itPj, param){
+        console.log('ZIP win32...');
+        zipFolder(
+          __dirname + '/build/pickles2desktoptool/win32/',
+          __dirname + '/build/Pickles2DesktopTool-win32.zip',
+          function(err) {
+            if(err) {
+                console.log('ERROR!', err);
+            } else {
+                console.log('success.');
+            }
+            itPj.next();
+        });
+      },
+      function(itPj, param){
+        console.log('cleanup...');
+        if( _utils.isDirectory(__dirname+'/build/pickles2desktoptool/') ){
+          _utils.rmdir_r(__dirname+'/build/pickles2desktoptool/');
+        }
+        itPj.next();
+      },
+      function(itPj, param){
+        console.log('all zip done!');
+        itPj.next();
+      }
+    ]).start({});
+
+  })();
+
 }).catch(function (error) {
   console.error(error);
 });

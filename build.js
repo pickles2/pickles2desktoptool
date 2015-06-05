ae4afbfcb55fe336b0f6175ffaaa6a47e5620e1e
Tomoya Koyanagi <tomk79@gmail.com>
2015-06-05 18:20:12 +0900

    -nb バージョンのビルドで、ZIPファイル名の後ろに日時をつけるようにした。

--- build.js
+++ build.js
@@ -2,20 +2,23 @@ var _utils = require('./app/index_files/_utils.node.js');
 var NwBuilder = require('node-webkit-builder');
 var zipFolder = require('zip-folder');
 var packageJson = require('./package.json');
-
+var phpjs = require('phpjs');
+var date = new Date();
 
 console.log('== build "Pickles 2 Desktop Tool" ==');
 
 console.log('Cleanup...');
-if( _utils.isDirectory(__dirname+'/build/pickles2desktoptool/') ){
-  _utils.rmdir_r(__dirname+'/build/pickles2desktoptool/');
-}
-if( _utils.isFile(__dirname+'/build/Pickles2DesktopTool-osx32.zip') ){
-  _utils.rm(__dirname+'/build/Pickles2DesktopTool-osx32.zip');
-}
-if( _utils.isFile(__dirname+'/build/Pickles2DesktopTool-win32.zip') ){
-  _utils.rm(__dirname+'/build/Pickles2DesktopTool-win32.zip');
-}
+(function(base){
+  var ls = _utils.ls(base);
+  for(var idx in ls){
+    if( base+'/'+ls[idx] == '.gitkeep' ){continue;}
+    if( _utils.isDirectory(base+'/'+ls[idx]) ){
+      _utils.rmdir_r(base+'/'+ls[idx]);
+    }else if( _utils.isFile(base+'/'+ls[idx]) ){
+      _utils.rm(base+'/'+ls[idx]);
+    }
+  }
+})( __dirname+'/build/' );
 console.log('');
 
 
@@ -43,13 +46,23 @@ nw.build().then(function () {
   console.log('all build done!');
 
   (function(){
+    var versionSign = packageJson.version;
+    function pad(str, len){
+      str += '';
+      str = phpjs.str_pad(str, len, '0', 'STR_PAD_LEFT');
+      return str;
+    }
+    if( packageJson.version.match(new RegExp('\-nb$')) ){
+      versionSign += '-'+pad(date.getFullYear(),4)+pad(date.getMonth()+1, 2)+pad(date.getDate(), 2);
+      versionSign += '-'+pad(date.getHours(),2)+pad(date.getMinutes(), 2);
+    }
 
     _utils.iterateFnc([
       function(itPj, param){
         console.log('ZIP mac32...');
         zipFolder(
           __dirname + '/build/pickles2desktoptool/osx32/',
-          __dirname + '/build/Pickles2DesktopTool-'+packageJson.version+'-osx32.zip',
+          __dirname + '/build/Pickles2DesktopTool-'+versionSign+'-osx32.zip',
           function(err) {
             if(err) {
                 console.log('ERROR!', err);
@@ -63,7 +76,7 @@ nw.build().then(function () {
         console.log('ZIP win32...');
         zipFolder(
           __dirname + '/build/pickles2desktoptool/win32/',
-          __dirname + '/build/Pickles2DesktopTool-'+packageJson.version+'-win32.zip',
+          __dirname + '/build/Pickles2DesktopTool-'+versionSign+'-win32.zip',
           function(err) {
             if(err) {
                 console.log('ERROR!', err);
@@ -75,9 +88,7 @@ nw.build().then(function () {
       },
       function(itPj, param){
         console.log('cleanup...');
-        if( _utils.isDirectory(__dirname+'/build/pickles2desktoptool/') ){
-          _utils.rmdir_r(__dirname+'/build/pickles2desktoptool/');
-        }
+        _utils.rmdir_r(__dirname+'/build/pickles2desktoptool/');
         itPj.next();
       },
       function(itPj, param){

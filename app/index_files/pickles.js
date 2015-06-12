6407bfb2139f4c2f546f99ac8a87d69fb9844f16
Tomoya Koyanagi <tomk79@gmail.com>
2015-06-12 13:10:41 +0900

    -nb

--- app/index_files/pickles.js
+++ app/index_files/pickles.js
@@ -34,7 +34,7 @@ new (function($, window){
 	 * @return string バージョン番号を示す文字列
 	 */
 	this.getVersion = function(){
-		return '2.0.0-beta5';
+		return '2.0.0-beta5-nb';
 	}
 
 	var _utils = require('./index_files/_utils.node.js');

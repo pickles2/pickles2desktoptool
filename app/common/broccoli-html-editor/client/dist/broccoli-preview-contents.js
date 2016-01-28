a9e5ef29776a5c97fb9cecdcc792d13c87bad9bc
Tomoya Koyanagi <tomk79@gmail.com>
2016-01-28 16:29:43 +0900

    broccoli: コンテンツエリア外のリンク、フォームを押した場合の遷移をブロックする機能追加。

--- app/common/broccoli-html-editor/client/dist/broccoli-preview-contents.js
+++ app/common/broccoli-html-editor/client/dist/broccoli-preview-contents.js
@@ -98,7 +98,7 @@
 			return;
 
 		}else if(data.api == 'getHtmlContentHeight'){
-			var height = $iframeWindow.outerHeight();
+			var height = $iframeWindow.height() + 16;
 			callbackMessage(data.callback, height);
 			return;
 
@@ -122,6 +122,25 @@
 			return;
 		}
 		return;
+	});
+
+	$iframeWindow.on("click", "a", function() {
+		var data = {};
+		var $this = $(this);
+		data.tagName = this.tagName.toLowerCase();
+		data.href = $this.attr('href');
+		data.target = $this.attr('target');
+		callbackMessage( 'onClickContentsLink', data );
+		return false;
+	});
+	$iframeWindow.find('form').bind("submit", function() {
+		var data = {};
+		var $this = $(this);
+		data.tagName = this.tagName.toLowerCase();
+		data.href = $this.attr('action');
+		data.target = $this.attr('target');
+		callbackMessage( 'onClickContentsLink', data );
+		return false;
 	});
 
 })();

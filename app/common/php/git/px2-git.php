0c10ac5124c555aa4af20035a7bc06f40e19be6c
Tomoya Koyanagi <tomk79@gmail.com>
2016-05-20 03:04:36 +0900

    tomk79/px2-git が最低限の疎通を達成

--- app/common/php/git/px2-git.php
+++ app/common/php/git/px2-git.php
@@ -2,5 +2,51 @@
 
 require_once(__DIR__.'/../../../../vendor/autoload.php');
 
-print 'hello world';
+class px2git{
+	/** px2-git object */
+	private $px2git;
+
+	/** method */
+	private $method;
+
+	/** entryScript path */
+	private $entryScript;
+
+	/**
+	 * constructor
+	 */
+	public function __construct(){
+		// var_dump($_SERVER['argv']);
+		$count = count($_SERVER['argv']);
+		// var_dump($count);
+		$count = $count-1;
+		// var_dump($count);
+		$arg = $_SERVER['argv'][$count];
+		$arg = base64_decode( $arg );
+		$arg = json_decode( $arg );
+		// var_dump($arg);
+
+		$this->entryScript = $arg->entryScript;
+		$this->method = $arg->method;
+		$this->px2git = new tomk79\pickles2\git\main( $this->entryScript );
+	}
+
+	/**
+	 * px2-git を実行する
+	 * @return void no return.
+	 */
+	public function execute(){
+		switch( $this->method ){
+			case 'commit_sitemap':
+				$this->px2git->commit_sitemap();
+				break;
+		}
+		return;
+	}
+}
+
+
+
+$obj = new px2git();
+$obj->execute();
 exit();
--- app/index_files/pickles.project.git.js
+++ app/index_files/pickles.project.git.js
@@ -5,13 +5,51 @@ module.exports = function( px, pj ) {
 	var _this = this;
 
 	this.nodePhpBin = px.nodePhpBin;
+	this.utils79 = px.utils79;
+	this.entryScript = require('path').resolve(pj.get('path'), pj.get('entry_script'));
 
 	/**
 	 * サイトマップをコミットする
 	 * @return {[type]} [description]
 	 */
-	this.commitSitemap = function(){
-		console.log('under construction.');
+	this.commitSitemap = function(callback){
+		callback = callback||function(){};
+		// console.log('under construction.');
+		var path_px2git = require('path').resolve(__dirname+'/../common/php/git/px2-git.php');
+		// console.log(path_px2git);
+
+		var param = {
+			'method': 'commit_sitemap',
+			'entryScript': this.entryScript
+		};
+
+		// PHPスクリプトを実行する
+		var rtn = '';
+		var err = '';
+		this.nodePhpBin.script(
+			[
+				path_px2git,
+				_this.utils79.base64_encode(JSON.stringify(param))
+			],
+			{
+				"success": function(data){
+					rtn += data;
+					console.log(data);
+				} ,
+				"error": function(data){
+					rtn += data;
+					err += data;
+					console.log(data);
+				} ,
+				"complete": function(data, error, code){
+					setTimeout(function(){
+						console.log(rtn, err, code);
+						callback();
+					},1000);
+				}
+			}
+		);
+		return;
 	}
 
 	return this;

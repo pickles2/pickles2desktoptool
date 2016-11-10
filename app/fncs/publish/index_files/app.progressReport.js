74ecc958ddda7a470cac3f6e4a099140896399bf
Tomoya Koyanagi <tomk79@gmail.com>
2016-11-10 18:07:34 +0900

    パブリッシュコマンドの発行を px2agent 依存に変更

--- app/fncs/publish/index_files/app.progressReport.js
+++ app/fncs/publish/index_files/app.progressReport.js
@@ -3,6 +3,7 @@
  */
 window.contApp.progressReport = new (function(px, $){
 	var _this = this;
+	var _pj = px.getCurrentProject();
 	var $results, $phase, $currentTask, $timer, $row, $progressBar;
 	var _timer;
 
@@ -23,74 +24,68 @@ window.contApp.progressReport = new (function(px, $){
 
 		var phase;
 
-		var _pathCurrentDir = px.process.cwd();
-		if(opts.cmdCd){
-			px.process.chdir( opts.cmdCd );
-		}
+		console.log(opts);
 
-		var proc = px.nodePhpBin.spawn(
-			opts.spawnCmdOpts ,
-			{}
-		);
-		proc.stdout.on('data', function(data){
-			var data = data.toString();
-			var rows = data.split(new RegExp('(\r\n|\r|\n)+'));
-			for( var idx in rows ){
-				var row = px.php.trim( rows[idx] );
-				if( typeof(row) !== typeof('') || !row.length ){
-					continue;
+		_pj.px2proj.publish({
+			"path_region": opts.path_region,
+			"paths_region": opts.paths_region,
+			"paths_ignore": opts.paths_ignore,
+			"is_keep_cache": opts.is_keep_cache,
+			"success": function(data){
+				// console.log(data);
+				try {
+					var data = data.toString();
+					var rows = data.split(new RegExp('(\r\n|\r|\n)+'));
+				} catch (e) {
 				}
-				if( row.match( new RegExp('^\\#\\#([\\s\\S]+)$') ) ){
-					phase = px.php.trim( RegExp.$1 );
-					if( phase == 'Start publishing' ){
-						$phase.text( 'Publishing...' );
-						(function(){
-							var startTimestamp = (new Date).getTime();
-							function updateTimer(){
-								var time = (new Date).getTime() - startTimestamp;
-								$timer.text( Math.floor(time/1000) + ' sec' );
-								_timer = setTimeout( updateTimer, 25 );
-							}
-							updateTimer();
-						})();
-					}else{
-						$phase.text( phase );
+				for( var idx in rows ){
+					var row = px.php.trim( rows[idx] );
+					if( typeof(row) !== typeof('') || !row.length ){
+						continue;
 					}
-				}else if( phase == 'Start publishing' ){
-					if( row.match( new RegExp('^([0-9]+)\\/([0-9]+)$') ) ){
-						$currentTask.text( RegExp.$1 +' / '+ RegExp.$2 );
-						var per = RegExp.$1/RegExp.$2*100;
-						$progressBar.attr({'aria-valuenow':per}).css({'width':per+'%'});
-					}else if( row.match( new RegExp('^\\/([\\s\\S]+)$') ) ){
+					if( row.match( new RegExp('^\\#\\#([\\s\\S]+)$') ) ){
+						phase = px.php.trim( RegExp.$1 );
+						if( phase == 'Start publishing' ){
+							$phase.text( 'Publishing...' );
+							(function(){
+								var startTimestamp = (new Date).getTime();
+								function updateTimer(){
+									var time = (new Date).getTime() - startTimestamp;
+									$timer.text( Math.floor(time/1000) + ' sec' );
+									_timer = setTimeout( updateTimer, 25 );
+								}
+								updateTimer();
+							})();
+						}else{
+							$phase.text( phase );
+						}
+					}else if( phase == 'Start publishing' ){
+						if( row.match( new RegExp('^([0-9]+)\\/([0-9]+)$') ) ){
+							$currentTask.text( RegExp.$1 +' / '+ RegExp.$2 );
+							var per = RegExp.$1/RegExp.$2*100;
+							$progressBar.attr({'aria-valuenow':per}).css({'width':per+'%'});
+						}else if( row.match( new RegExp('^\\/([\\s\\S]+)$') ) ){
+							$row.text(row);
+						}
+					}else if( phase == 'Clearing caches' ){
 						$row.text(row);
+					}else if( phase == 'Making list' ){
+						$row.text(row);
+					}else{
+						$row.text('');
 					}
-				}else if( phase == 'Clearing caches' ){
-					$row.text(row);
-				}else if( phase == 'Making list' ){
-					$row.text(row);
-				}else{
-					$row.text('');
+					// console.log( row );
 				}
-				// console.log( row );
+			},
+			"complete":function(data){
+				// console.log(data);
+				clearTimeout(_timer);
+				setTimeout(function(){
+					px.progress.close();
+					opts.complete(true);
+				}, 3000);
 			}
 		});
-		proc.stderr.on('data', function(data){
-			var row = new data.toString();
-			row = px.php.trim( row );
-			console.log( 'ERROR: '+ row );
-		});
-		proc.on('close', function(code){
-			// console.log( code );
-			clearTimeout(_timer);
-			setTimeout(function(){
-				px.progress.close();
-				opts.complete(true);
-			}, 3000);
-		});
-
-		if(opts.cmdCd){
-			px.process.chdir( _pathCurrentDir );
-		}
 
 	}// this.init();
 

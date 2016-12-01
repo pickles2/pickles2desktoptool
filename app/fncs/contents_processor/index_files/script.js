a60883b01598314848937686b59fcd01679d4d4e
Tomoya Koyanagi <tomk79@gmail.com>
2016-12-01 22:59:54 +0900

    コンテンツ一括加工機能で、GUI編集コンテンツ以外も変換処理を通るようになった。

--- app/fncs/contents_processor/index_files/script.js
+++ app/fncs/contents_processor/index_files/script.js
@@ -48,112 +48,109 @@ window.contApp = new (function(px){
 	var processor = function(script_source_processor, script_instance_processor, callback){
 		// console.log(script_source_processor, script_instance_processor);
 
-		function srcProcessor( html, next ){
+		function srcProcessor( src, type, next ){
 			try {
 				eval(script_source_processor.toString());
 			} catch (e) {
 				console.log('eval ERROR');
-				next();
+				next(src);
 			}
 		}
 
-		pj.createSearcher().getGuiEditPages( function(pageList){
+		var pageList = pj.site.getSitemap();
 
-			px.utils.iterate(
-				pageList ,
-				function( it1, sitemapRow, idx1 ){
-					// console.log(sitemapRow);
-					$pre.text( $pre.text() + sitemapRow.path );
+		px.utils.iterate(
+			pageList ,
+			function( it1, sitemapRow, idx1 ){
+				// console.log(sitemapRow);
+				$pre.text( $pre.text() + sitemapRow.path );
 
-					px.it79.fnc(
-						{"pageInfo": sitemapRow},
-						[
-							function(it2, arg2){
-								// HTMLコンテンツのみ抽出
-								var procType = pj.get_path_proc_type( arg2.pageInfo.path );
+				px.it79.fnc(
+					{"pageInfo": sitemapRow},
+					[
+						function(it2, arg2){
+							// HTML拡張子のみ抽出
+							var procType = pj.get_path_proc_type( arg2.pageInfo.path );
+							$pre.text( $pre.text() + ' -> ' + procType );
+							switch( procType ){
+								case 'html':
+								case 'htm':
+									it2.next(arg2);
+									break;
+								default:
+									$pre.text( $pre.text() + ' -> SKIP' );
+									$pre.text( $pre.text() + "\n" );
+									it1.next();
+									break;
+							}
+						} ,
+						function(it2, arg2){
+							pj.getPageContentEditorMode( arg2.pageInfo.path, function(procType){
 								$pre.text( $pre.text() + ' -> ' + procType );
 								switch( procType ){
-									case 'html':
-									case 'htm':
-										it2.next(arg2);
-										break;
-									default:
+									case '.not_exists':
 										$pre.text( $pre.text() + ' -> SKIP' );
 										$pre.text( $pre.text() + "\n" );
 										it1.next();
 										break;
-								}
-							} ,
-							function(it2, arg2){
-								// そのうち、GUI編集コンテンツのみ抽出
-								pj.getPageContentEditorMode( arg2.pageInfo.path, function(procType){
-									$pre.text( $pre.text() + ' -> ' + procType );
-									switch( procType ){
-										case 'html.gui':
-											it2.next(arg2);
-											break;
-										default:
-											$pre.text( $pre.text() + ' -> SKIP' );
+
+									case 'html.gui':
+										// broccoli-processor オブジェクトを生成する
+										// console.log(arg2.pageInfo.path);
+										pj.createBroccoliProcessor( arg2.pageInfo.path, function(broccoliProcessor){
+											// console.log(broccoliProcessor);
+
+											broccoliProcessor
+												.each(
+													function( editor ){
+														// console.log(data);
+														// next();
+														try {
+															eval(script_instance_processor.toString());
+														} catch (e) {
+															console.log('eval ERROR', e);
+															editor.done();
+														}
+													}
+												)
+												.run(function(logs){
+													console.log(arg2.pageInfo.path, logs);
+													console.log('replace done!');
+													$pre.text( $pre.text() + ' -> done' );
+													$pre.text( $pre.text() + "\n" );
+													it2.next(arg2);
+												})
+											;
+
+										} );
+										break;
+
+									case 'html':
+									case 'md':
+									default:
+										srcProcessor( 'src', procType, function(after){
+											$pre.text( $pre.text() + ' -> done' );
 											$pre.text( $pre.text() + "\n" );
-											it1.next();
-											break;
-									}
-								} );
-							} ,
-							function(it2, arg2){
-								// broccoli-processor オブジェクトを生成する
-								// console.log(arg2.pageInfo.path);
-								pj.createBroccoliProcessor( arg2.pageInfo.path, function(broccoliProcessor){
-									// console.log(broccoliProcessor);
-
-									broccoliProcessor
-										.each(
-											function( editor ){
-												// console.log(data);
-												// next();
-												try {
-													eval(script_instance_processor.toString());
-												} catch (e) {
-													console.log('eval ERROR', e);
-													editor.done();
-												}
-											}
-										)
-										.run(function(logs){
-											console.log(arg2.pageInfo.path, logs);
-											console.log('replace done!');
 											it2.next(arg2);
-										})
-									;
+										} );
+										break;
 
-								} );
-							} ,
-							function(it2, arg2){
-								// broccoli コンテンツを再生成する
-								pj.buildGuiEditContent( arg2.pageInfo.path, function(result){
-									if(result){
-										$pre.text( $pre.text() + ' -> done' );
-									}else{
-										$pre.text( $pre.text() + ' -> ERROR!' );
-									}
-									$pre.text( $pre.text() + "\n" );
-									it2.next(arg2);
-								} );
-							} ,
-							function(it2, arg2){
-								it1.next();
-							}
-						]
-					);
+								}
+							} );
+						} ,
+						function(it2, arg2){
+							it1.next();
+						}
+					]
+				);
 
-				} ,
-				function(){
-					px.message( '完了しました。' );
-					callback();
-				}
-			);
+			} ,
+			function(){
+				px.message( '完了しました。' );
+				callback();
+			}
+		);
 
-		} );
 	}
 
 	/**

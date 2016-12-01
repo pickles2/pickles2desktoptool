b62055347ae72f5fc5dfc72982cad669f234cc09
Tomoya Koyanagi <tomk79@gmail.com>
2016-12-01 21:29:49 +0900

    コンテンツ一括加工機能をざっくり実装した。

--- app/fncs/contents_processor/index_files/script.js
+++ app/fncs/contents_processor/index_files/script.js
@@ -10,60 +10,71 @@ window.contApp = new (function(px){
 	 */
 	function init(){
 		$cont = $('.contents').html('');
-		$btn = $('<button class="px2-btn px2-btn--block">');
-		$pre = $('<pre>');
 
-		$cont
-			.append( $btn
-				.click( function(){ rebuild(this); } )
-				.text('すべてのコンテンツを一括加工する')
-			)
-			.append( $pre
-				.addClass( 'cont_console' )
-				.css({
-					'max-height': 360,
-					'height': 360
-				})
-			)
-		;
+		var html = $('#template-main-form').html();
+		$cont.html(html);
+		$btn = $cont.find('button');
+		$pre = $cont.find('pre');
+
+		$btn
+			.click( function(){
+				var btn = this;
+				var $form = $cont.find('form');
+				var script_source_processor = $form.find('textarea[name=script_source_processor]').val();
+				var script_instance_processor = $form.find('textarea[name=script_instance_processor]').val();
+				$pre.text('');
+				$(btn).attr('disabled', 'disabled');
+				$form.find('textarea[name=script_source_processor]').attr('disabled', 'disabled');
+				$form.find('textarea[name=script_instance_processor]').attr('disabled', 'disabled');
+				processor(
+					script_source_processor,
+					script_instance_processor,
+					function(){
+						$pre.text( $pre.text() + 'completed!' );
+						$(btn).removeAttr('disabled').focus();
+						$form.find('textarea[name=script_source_processor]').removeAttr('disabled');
+						$form.find('textarea[name=script_instance_processor]').removeAttr('disabled');
+					}
+				);
+			} );
+		$pre
+			.css({
+				'max-height': 360,
+				'height': 360
+			});
 	}
 
 
-	var rebuild = function(btn){
-		$(btn).attr('disabled', 'disabled');
-		$pre.text('');
+	var processor = function(script_source_processor, script_instance_processor, callback){
+		// console.log(script_source_processor, script_instance_processor);
+
+		function srcProcessor( html, next ){
+			try {
+				eval(script_source_processor.toString());
+			} catch (e) {
+				console.log('eval ERROR');
+				next();
+			}
+		}
 
 		pj.createSearcher().getGuiEditPages( function(pageList){
 
 			px.utils.iterate(
 				pageList ,
 				function( it1, sitemapRow, idx1 ){
-					console.log(sitemapRow);
+					// console.log(sitemapRow);
 					$pre.text( $pre.text() + sitemapRow.path );
 
-					px.utils.iterateFnc([
-						function(it2, arg2){
-							// HTMLコンテンツのみ抽出
-							var procType = pj.get_path_proc_type( arg2.pageInfo.path );
-							$pre.text( $pre.text() + ' -> ' + procType );
-							switch( procType ){
-								case 'html':
-								case 'htm':
-									it2.next(arg2);
-									break;
-								default:
-									$pre.text( $pre.text() + ' -> SKIP' );
-									$pre.text( $pre.text() + "\n" );
-									it1.next();
-									break;
-							}
-						} ,
-						function(it2, arg2){
-							// そのうち、GUI編集コンテンツのみ抽出
-							pj.getPageContentEditorMode( arg2.pageInfo.path, function(procType){
+					px.it79.fnc(
+						{"pageInfo": sitemapRow},
+						[
+							function(it2, arg2){
+								// HTMLコンテンツのみ抽出
+								var procType = pj.get_path_proc_type( arg2.pageInfo.path );
 								$pre.text( $pre.text() + ' -> ' + procType );
 								switch( procType ){
-									case 'html.gui':
+									case 'html':
+									case 'htm':
 										it2.next(arg2);
 										break;
 									default:
@@ -72,37 +83,73 @@ window.contApp = new (function(px){
 										it1.next();
 										break;
 								}
-							} );
-						} ,
-						function(it2, arg2){
-							// broccoli-processor オブジェクトを生成する
-							pj.createBroccoliProcessor( arg2.pageInfo.path, function(broccoliProcessor){
-								console.log(broccoliProcessor);
-								it2.next(arg2);
-							} );
-						} ,
-						function(it2, arg2){
-							// broccoli コンテンツを再生成する
-							pj.buildGuiEditContent( arg2.pageInfo.path, function(result){
-								if(result){
-									$pre.text( $pre.text() + ' -> done' );
-								}else{
-									$pre.text( $pre.text() + ' -> ERROR!' );
-								}
-								$pre.text( $pre.text() + "\n" );
-								it2.next(arg2);
-							} );
-						} ,
-						function(it2, arg2){
-							it1.next();
-						}
-					]).start({"pageInfo": sitemapRow});
+							} ,
+							function(it2, arg2){
+								// そのうち、GUI編集コンテンツのみ抽出
+								pj.getPageContentEditorMode( arg2.pageInfo.path, function(procType){
+									$pre.text( $pre.text() + ' -> ' + procType );
+									switch( procType ){
+										case 'html.gui':
+											it2.next(arg2);
+											break;
+										default:
+											$pre.text( $pre.text() + ' -> SKIP' );
+											$pre.text( $pre.text() + "\n" );
+											it1.next();
+											break;
+									}
+								} );
+							} ,
+							function(it2, arg2){
+								// broccoli-processor オブジェクトを生成する
+								// console.log(arg2.pageInfo.path);
+								pj.createBroccoliProcessor( arg2.pageInfo.path, function(broccoliProcessor){
+									// console.log(broccoliProcessor);
+
+									broccoliProcessor
+										.each(
+											function( editor ){
+												// console.log(data);
+												// next();
+												try {
+													eval(script_instance_processor.toString());
+												} catch (e) {
+													console.log('eval ERROR', e);
+													editor.done();
+												}
+											}
+										)
+										.run(function(logs){
+											console.log(arg2.pageInfo.path, logs);
+											console.log('replace done!');
+											it2.next(arg2);
+										})
+									;
+
+								} );
+							} ,
+							function(it2, arg2){
+								// broccoli コンテンツを再生成する
+								pj.buildGuiEditContent( arg2.pageInfo.path, function(result){
+									if(result){
+										$pre.text( $pre.text() + ' -> done' );
+									}else{
+										$pre.text( $pre.text() + ' -> ERROR!' );
+									}
+									$pre.text( $pre.text() + "\n" );
+									it2.next(arg2);
+								} );
+							} ,
+							function(it2, arg2){
+								it1.next();
+							}
+						]
+					);
 
 				} ,
 				function(){
-					$pre.text( $pre.text() + 'completed!' );
-					$(btn).removeAttr('disabled').focus();
 					px.message( '完了しました。' );
+					callback();
 				}
 			);
 

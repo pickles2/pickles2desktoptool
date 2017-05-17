649a4a965115a1c9e6aa0060f11cf3c92c028b9a
Tomoya Koyanagi <tomk79@gmail.com>
2017-05-17 15:54:13 +0900

    同名で拡張子が異なるサイトマップファイルをまとめて表示するように変更した。

--- src/fncs/sitemap/index_files/app.js
+++ src/fncs/sitemap/index_files/app.js
@@ -1,46 +1,150 @@
 window.px = window.parent.px;
 window.contApp = new (function(px, $){
 	var _this = this;
+	var it79 = require('iterate79');
+	var utils79 = require('utils79');
 	var pj = px.getCurrentProject();
-	var filelist = pj.getSitemapFilelist();
+	var realpath_sitemap_dir = pj.get('path')+'/'+pj.get('home_dir')+'/sitemaps/';
+	var filelist;
+	var $filelist;
 	this.git = pj.git();
 	// console.log(git);
 	this.gitUi = new px2dtGitUi(px, pj);
 
 	/**
 	 * initialize
-	 * @return {void} no return;
 	 */
-	function init(){
+	function init( callback ){
+		callback = callback || function(){};
 
-		$('.cont_filelist_sitemap')
-			.html('')
-			.append( $('<ul>').addClass('listview') )
-		;
+		it79.fnc({}, [
+			function(it1, arg){
+				$filelist = $('.cont_filelist_sitemap');
+				filelist = {};
+				it1.next(arg);
+			},
+			function(it1, arg){
+				// --------------------------------------
+				// サイトマップファイル名の一覧を整理
+				var filelist_original = pj.getSitemapFilelist();
+				filelistLoop:for( var idx in filelist_original ){
+					var filename_orig = filelist_original[idx];
+					var filename = filename_orig.replace(/\.[a-zA-Z0-9]+$/, '');
+					var ext = px.utils.getExtension(filename_orig).toLowerCase();
+					switch( ext ){
+						case 'csv':
+						case 'xlsx':
+							break;
+						default:
+							continue filelistLoop;
+					}
 
-		filelistLoop:for( var i in filelist ){
-			switch( px.utils.getExtension(filelist[i]) ){
-				case 'csv':
-				case 'xlsx':
-					break;
-				default:
-					continue filelistLoop;
+					try {
+						if( !filelist[filename] ){
+							filelist[filename] = {};
+						}
+						filelist[filename].basefilename = filename;
+						filelist[filename].masterExt = ext;
+						filelist[filename].masterFilename = filename_orig;
+						if( !filelist[filename].exts ){
+							filelist[filename].exts = {};
+						}
+						if( !filelist[filename].exts[ext] ){
+							filelist[filename].exts[ext] = {};
+						}
+						filelist[filename].exts[ext].basefilename = filename;
+						filelist[filename].exts[ext].filename = filename_orig;
+						filelist[filename].exts[ext].ext = ext.toLowerCase();
+					} catch (e) {
+						console.error('[ERROR] An error occurred while sorting Sitemap file list. -> on "'+filename+'".', e);
+					}
+
+				}
+				// console.log(filelist);
+				it1.next(arg);
+			},
+			function(it1, arg){
+				// --------------------------------------
+				// サイトマップファイルのマスターフォーマットを決める
+				for( var idx in filelist ){
+					var masterExt = null;
+					try {
+						// 本来は、 px2-sitemapexcel のオプションを確認して、
+						// マスターファイルの設定をファイルごとに決めたいところ。
+						// いまのところは固定的に xlsx をマスターとして優先する。
+						if( filelist[idx].exts.xlsx ){
+							masterExt = 'xlsx';
+						}else if( filelist[idx].exts.csv ){
+							masterExt = 'csv';
+						}
+					} catch (e) {
+					}
+					// masterExt = 'csv';
+					filelist[idx].masterExt = masterExt;
+					filelist[idx].masterFilename = filelist[idx].exts[masterExt].filename;
+				}
+				it1.next(arg);
+			},
+			function(it1, arg){
+				// --------------------------------------
+				// 一覧を描画
+				var $ul = $('<ul>').addClass('listview');
+
+				for( var idx in filelist ){
+
+					var $a = $('<a>');
+
+					$ul.append( $('<li>')
+						.append( $a
+							.attr({
+								'href': 'javascript:;',
+								'data-filename': filelist[idx].masterFilename,
+								'data-num': idx
+							})
+							.on('click', function(e){
+								var path = realpath_sitemap_dir+$(this).attr('data-filename');
+								px.utils.openURL( path );
+							} )
+							.text( filelist[idx].masterFilename )
+						)
+					);
+
+					var $extUl = $('<ul>').addClass('cont_filelist_sitemap__ext-list');
+					$a.append($extUl);
+
+					for( var ext in filelist[idx].exts ){
+						var $aExt = $('<a>');
+						var $li = $('<li>')
+							.append( $aExt
+								.addClass('px2-btn')
+								.attr({
+									'href': 'javascript:;',
+									'data-filename': filelist[idx].exts[ext].filename
+								})
+								.on('click', function(e){
+									var path = realpath_sitemap_dir+$(this).attr('data-filename');
+									px.utils.openURL( path );
+								})
+								.text( filelist[idx].exts[ext].ext )
+							)
+						;
+						if( ext == filelist[idx].masterExt ){
+							$aExt.addClass('px2-btn--primary');
+						}
+						$extUl.append($li);
+					}
+				}
+
+				$filelist
+					.html('')
+					.append( $ul )
+				;
+				it1.next(arg);
+			},
+			function(it1, arg){
+				callback();
 			}
-
-			$('.cont_filelist_sitemap ul').append( $('<li>')
-				.append( $('<a>')
-					.attr('href', 'javascript:;')
-					.data('filename', filelist[i] )
-					.data('num', i)
-					.click( function(e){
-						var path = pj.get('path')+'/'+pj.get('home_dir')+'/sitemaps/'+$(this).data('filename');
-						px.utils.openURL( path );
-					} )
-					.text( filelist[i] )
-				)
-			);
-		}
-		// $('.cont_filelist_sitemap ul').listview();
+		]);
 
 	}
 
@@ -68,7 +172,7 @@ window.contApp = new (function(px, $){
 	 * フォルダを開く
 	 */
 	this.openInFinder = function(){
-		px.utils.openURL( pj.get('path')+'/'+pj.get('home_dir')+'/sitemaps/' );
+		px.utils.openURL( realpath_sitemap_dir );
 	}
 
 	/**

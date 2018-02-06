6aa4091f2f1b381a323be3af731cf0f58fcc9873
Tomoya Koyanagi <tomk79@gmail.com>
2018-02-06 19:41:35 +0900

    (update submodule: node-pickles2-contents-editor)

--- app/common/pickles2-contents-editor/dist/pickles2-contents-editor.js
+++ app/common/pickles2-contents-editor/dist/pickles2-contents-editor.js
@@ -22391,6 +22391,7 @@ module.exports = function(px2ce){
 			var pathname = px2conf.path_controot + page_path;
 			pathname = pathname.replace( new RegExp('\/+', 'g'), '/' );
 			pathname += '?THEME='+encodeURIComponent(px2ce.theme_id);
+			pathname += '&LAYOUT='+encodeURIComponent(px2ce.layout_id);
 			return px2ce.options.preview.origin + pathname;
 		}
 
@@ -22719,6 +22720,7 @@ module.exports = function(px2ce){
 			var pathname = px2conf.path_controot + page_path;
 			pathname = pathname.replace( new RegExp('\/+', 'g'), '/' );
 			pathname += '?THEME='+encodeURIComponent(px2ce.theme_id);
+			pathname += '&LAYOUT='+encodeURIComponent(px2ce.layout_id);
 			return px2ce.options.preview.origin + pathname;
 		}
 		var pathname = px2conf.path_controot + px2ce.page_path;

47d8b37e92a0b41f0c6c48c6a0bedc7f9e097f88
Tomoya Koyanagi <tomk79@gmail.com>
2018-11-27 17:37:20 +0900

    <Files And Folders> HTMLファイル作成時に GUI編集モード にするか尋ねるようになった。

--- app/fncs/files_and_folders/index_files/script.js
+++ app/fncs/files_and_folders/index_files/script.js
@@ -319,12 +319,55 @@ window.contApp = new (function( px ){
 						px2style.closeModal();
 						var filename = $body.find('[name=filename]').val();
 						if( !filename ){ return; }
-						callback( filename );
+						var pageInfoAll;
+
+						px.it79.fnc({}, [
+							function(it1){
+								_pj.execPx2(
+									current_dir+filename+'?PX=px2dthelper.get.all',
+									{
+										complete: function(resources){
+											try{
+												resources = JSON.parse(resources);
+											}catch(e){
+												console.error('Failed to parse JSON "client_resources".', e);
+											}
+											// console.log(resources);
+											pageInfoAll = resources;
+											it1.next();
+										}
+									}
+								);
+
+							},
+							function(it1){
+								if( filename.match(/\.html?$/i) && $body.find('[name=is_guieditor]:checked').val() ){
+									// GUI編集モードが有効
+									var realpath_data_dir = pageInfoAll.realpath_data_dir;
+									px.fsEx.mkdirpSync( realpath_data_dir );
+									px.fs.writeFileSync( realpath_data_dir+'data.json', '{}' );
+								}
+								it1.next();
+							},
+							function(it1){
+								callback( filename );
+								it1.next();
+							}
+						]);
+
 					}
 					$body.find('form').on('submit', function(){
 						submitForm();
 					});
 					$body.find('.cont_current_dir').text(current_dir);
+					$body.find('[name=filename]').on('change keyup', function(){
+						var filename = $body.find('[name=filename]').val();
+						if( filename.match(/\.html?$/i) ){
+							$body.find('.cont_html_ext_option').show();
+						}else{
+							$body.find('.cont_html_ext_option').hide();
+						}
+					})
 					px2style.modal({
 						'title': 'Create new File',
 						'body': $body,

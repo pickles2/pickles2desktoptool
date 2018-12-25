70048a7dfd1112ee85f211a19ef0813425e94faf
Tomoya Koyanagi <tomk79@gmail.com>
2018-12-25 20:38:32 +0900

    (<MakeContentFilesByList> 不要な記述を削除)

--- src/fncs/make_content_files_by_list/index_files/script.js
+++ src/fncs/make_content_files_by_list/index_files/script.js
@@ -2,146 +2,18 @@ window.px = window.parent.px;
 window.contApp = new (function( px ){
 	var _this = this;
 	var _pj = px.getCurrentProject();
-	var remoteFinder;
 	var $elms = {};
 	$elms.editor = $('<div>');
-	var mkfile = new (require('../../../fncs/make_content_files_by_list/index_files/libs.ignore/mkfile.js'))(this, px, _pj, $);
-	var mkdir = new (require('../../../fncs/make_content_files_by_list/index_files/libs.ignore/mkdir.js'))(this, px, _pj, $);
-	var open = new (require('../../../fncs/make_content_files_by_list/index_files/libs.ignore/open.js'))(this, px, _pj, $);
-	var copy = new (require('../../../fncs/make_content_files_by_list/index_files/libs.ignore/copy.js'))(this, px, _pj, $);
-	var rename = new (require('../../../fncs/make_content_files_by_list/index_files/libs.ignore/rename.js'))(this, px, _pj, $);
-	var remove = new (require('../../../fncs/make_content_files_by_list/index_files/libs.ignore/remove.js'))(this, px, _pj, $);
 
 	/**
 	 * 初期化
 	 */
 	$(window).on('load', function(){
-		remoteFinder = new RemoteFinder(
-			document.getElementById('cont_finder'),
-			{
-				"gpiBridge": function(input, callback){
-					// console.log(input);
-					_pj.remoteFinder.gpi(input, function(result){
-						callback(result);
-					});
-				},
-				"mkfile": mkfile.mkfile,
-				"mkdir": mkdir.mkdir,
-				"open": open.open,
-				"copy": copy.copy,
-				"rename": rename.rename,
-				"remove": remove.remove
-			}
-		);
-		// console.log(remoteFinder);
-		remoteFinder.init('/', {}, function(){
-			console.log('ready.');
-		});
 
 		$(window).on('resize', function(){
 			onWindowResize();
 		});
 	});
-
-	/**
-	 * エディター画面を開く
-	 */
-	this.openEditor = function( pagePath ){
-
-		this.closeEditor();//一旦閉じる
-
-		// プログレスモード表示
-		px.progress.start({
-			'blindness':true,
-			'showProgressBar': true
-		});
-
-		var contPath = _pj.findPageContent( pagePath );
-		var contRealpath = _pj.get('path')+'/'+contPath;
-		var pathInfo = px.utils.parsePath(contPath);
-		if( _pj.site.getPathType( pagePath ) == 'dynamic' ){
-			var dynamicPathInfo = _pj.site.get_dynamic_path_info(pagePath);
-			pagePath = dynamicPathInfo.path;
-		}
-
-		if( px.fs.existsSync( contRealpath ) ){
-			contRealpath = px.fs.realpathSync( contRealpath );
-		}
-
-		$elms.editor = $('<div>')
-			.css({
-				'position':'fixed',
-				'top':0,
-				'left':0 ,
-				'z-index': '1000',
-				'width':'100%',
-				'height':$(window).height()
-			})
-			.append(
-				$('<iframe>')
-					//↓エディタ自体は別のHTMLで実装
-					.attr( 'src', '../../mods/editor/index.html'
-						+'?page_path='+encodeURIComponent( pagePath )
-					)
-					.css({
-						'border':'0px none',
-						'width':'100%',
-						'height':'100%'
-					})
-			)
-			.append(
-				$('<a>')
-					.html('&times;')
-					.attr('href', 'javascript:;')
-					.on('click', function(){
-						// if(!confirm('編集中の内容は破棄されます。エディタを閉じますか？')){ return false; }
-						_this.closeEditor();
-					} )
-					.css({
-						'position':'absolute',
-						'bottom':5,
-						'right':5,
-						'font-size':'18px',
-						'color':'#333',
-						'background-color':'#eee',
-						'border-radius':'0.5em',
-						'border':'1px solid #333',
-						'text-align':'center',
-						'opacity':0.4,
-						'width':'1.5em',
-						'height':'1.5em',
-						'text-decoration': 'none'
-					})
-					.hover(function(){
-						$(this).animate({
-							'opacity':1
-						});
-					}, function(){
-						$(this).animate({
-							'opacity':0.4
-						});
-					})
-			)
-		;
-		$('body')
-			.append($elms.editor)
-			.css({'overflow':'hidden'})
-		;
-
-		return this;
-	} // openEditor()
-
-	/**
-	 * エディター画面を閉じる
-	 * 単に閉じるだけです。編集内容の保存などの処理は、editor.html 側に委ねます。
-	 */
-	this.closeEditor = function(){
-		$elms.editor.remove();
-		$('body')
-			.css({'overflow':'auto'})
-		;
-		return this;
-	} // closeEditor()
 
 	/**
 	 * ウィンドウリサイズイベントハンドラ

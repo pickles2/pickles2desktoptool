a8e133098aa630bc05b0e85981b9949cdcb591ff
Tomoya Koyanagi <tomk79@gmail.com>
2018-12-28 22:32:16 +0900

    <make content file list> 対象範囲、除外範囲をしていできるようになった

--- app/fncs/make_content_file_list/index_files/script.js
+++ app/fncs/make_content_file_list/index_files/script.js
@@ -12,21 +12,14 @@ window.contApp = new (function( px ){
 		.css({
 			'width':'100%'
 		});
-	var styleResult = {
-		'success' : {
-			'background-color': '#dfd'
-		},
-		'failed': {
-			'background-color': '#f99'
-		}
-	};
-	// var entryScript = require('path').resolve(_pj.get('path'), _pj.get('entry_script'));
 	var pathCsv;
 	var counter = 0;
 	var csvRowTemplate = JSON.stringify({
 		'path': '',
 		'filesize': ''
 	});
+	var paths_region_dir = [];
+	var paths_ignore_dir = [];
 
 	/**
 	 * 初期化
@@ -44,6 +37,9 @@ window.contApp = new (function( px ){
 
 		// ファイルリスト作成処理を実行
 		$main.find('form').on('submit', function(e){
+			paths_region_dir = parse_paths($main.find('textarea[name=paths_region_dir]').val());
+			paths_ignore_dir = parse_paths($main.find('textarea[name=paths_ignore_dir]').val());
+
 			$main.find('button').attr("disabled", "disabled");
 			var $btnCompolete = $('<button class="px2-btn px2-btn--primary">');
 			var $canvasContent = $('<div>');
@@ -94,6 +90,47 @@ window.contApp = new (function( px ){
 
 		});
 
+		_pj.px2proj.query(
+			'/?PX=px2dthelper.get.all',
+			{
+				"output": "json",
+				"complete": function(data, code){
+					// console.log(data, code);
+					var allInfo = JSON.parse(data);
+					// console.log(allInfo);
+
+					var regionList = [];
+					var ignoreList = [];
+					if(allInfo.realpath_docroot){
+						regionList.push(realpath2path(allInfo.realpath_docroot));
+					}
+					if(allInfo.packages.path_composer_root_dir){
+						ignoreList.push(realpath2path(allInfo.packages.path_composer_root_dir+'vendor/'));
+					}
+					if(allInfo.packages.path_npm_root_dir){
+						ignoreList.push(realpath2path(allInfo.packages.path_npm_root_dir+'node_modules/'));
+					}
+					if(allInfo.realpath_homedir){
+						ignoreList.push(realpath2path(allInfo.realpath_homedir));
+					}
+					if(allInfo.realpath_theme_collection_dir){
+						ignoreList.push(realpath2path(allInfo.realpath_theme_collection_dir));
+					}
+					var gitRoot = _pj.get_realpath_git_root();
+					if(gitRoot){
+						ignoreList.push(realpath2path(gitRoot+'.git/'));
+					}
+
+					// console.log(regionList);
+					// console.log(ignoreList);
+					$main.find('textarea[name=paths_region_dir]').val( regionList.join("\n") );
+					$main.find('textarea[name=paths_ignore_dir]').val( ignoreList.join("\n") );
+					return;
+				}
+			}
+		);
+
+
 		$(window).on('resize', function(){
 			onWindowResize();
 		});
@@ -112,26 +149,48 @@ window.contApp = new (function( px ){
 				// console.log(idx, path + basename);
 
 				if( px.utils79.is_file( path_base+path+basename ) ){
+					if(!is_target_path(path+basename)){
+						it1.next();
+						return;
+					}
 					var rowTemplate = JSON.parse(csvRowTemplate);
 					rowTemplate.path = path+basename;
 					counter ++;
 					$progress.html(counter);
 
-					var stat = px.fs.statSync(path_base+path+basename);
-					rowTemplate.filesize = stat.size;
 
-					// CSV行データを保存
-					var tmpStrRow = '';
-					for(var idx in rowTemplate){
-						var cell = rowTemplate[idx] + "";
-						cell = cell.split('"').join('""');
-						tmpStrRow += '"'+cell+'"' + ',';
-					}
-					px.fs.appendFileSync( pathCsv, tmpStrRow + "\n" );
+					_pj.px2proj.query(
+						path+basename + '?PX=px2dthelper.get.all',
+						{
+							"output": "json",
+							"complete": function(data, code){
+								// console.log(data, code);
+								var result = JSON.parse(data);
+								console.log(result);
 
-					it1.next();
+								var stat = px.fs.statSync(path_base+path+basename);
+								rowTemplate.filesize = stat.size;
+
+								// CSV行データを保存
+								var tmpStrRow = '';
+								for(var idx in rowTemplate){
+									var cell = rowTemplate[idx] + "";
+									cell = cell.split('"').join('""');
+									tmpStrRow += '"'+cell+'"' + ',';
+								}
+								px.fs.appendFileSync( pathCsv, tmpStrRow + "\n" );
+
+								it1.next();
+								return;
+							}
+						}
+					);
 					return;
 				}else if( px.utils79.is_dir( path_base+path+basename ) ){
+					if(!is_target_path(path+basename+'/')){
+						it1.next();
+						return;
+					}
 					scanDir( path + basename + '/', function(){
 						it1.next();
 					} );
@@ -139,12 +198,57 @@ window.contApp = new (function( px ){
 				}
 			},
 			function(){
-				setTimeout(function(){
-					callback();
-				}, 10);
+				callback();
 			}
 		);
 		return;
+	}
+
+	/**
+	 * パスの指定を解析する
+	 */
+	function parse_paths(strPaths){
+		var paths = strPaths.split(/[\r\n\,]+/);
+		for( var i in paths ){
+			paths[i] = px.utils79.trim(paths[i]);
+		}
+		return paths;
+	}
+
+	/**
+	 * パスが対象となるディレクトリか調べる
+	 */
+	function is_target_path(path){
+		// 対象範囲に含まれない場合
+		for(var i in paths_region_dir){
+			if( path.indexOf( paths_region_dir[i] ) === -1 ){
+				return false;
+			}
+		}
+
+		// 除外範囲に含まれる場合
+		for(var i in paths_ignore_dir){
+			if( path.indexOf( paths_ignore_dir[i] ) === 0 ){
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * 絶対パスから、ローカルパス部分を抽出する
+	 */
+	function realpath2path( realpath ){
+		if( typeof(realpath) !== typeof('') ){
+			return realpath;
+		}
+		var path = px.utils79.normalize_path(realpath);
+		var pjPath = px.utils79.normalize_path(_pj.get('path'));
+		if( path.indexOf( pjPath ) === 0 ){
+			path = path.substr(pjPath.length);
+		}
+		return path;
 	}
 
 	/**

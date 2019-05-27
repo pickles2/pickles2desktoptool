e64c6a6efd9221c4d3f7c890d83aabcaf2093309
Tomoya Koyanagi <tomk79@gmail.com>
2019-05-27 16:18:10 +0900

    (update libs)

--- app/common/px2style/dist/scripts.js
+++ app/common/px2style/dist/scripts.js
@@ -9819,10 +9819,11 @@ return jQuery;
     var Px2style = function(){};
     var px2style = window.px2style = new Px2style;
     var modal = require('./modal/_modal.js')(Px2style);
+    var loading = require('./styles/_loading.js')(Px2style);
     var header = require('./header/_header.js')(Px2style);
 })();
 
-},{"./header/_header.js":3,"./modal/_modal.js":4}],3:[function(require,module,exports){
+},{"./header/_header.js":3,"./modal/_modal.js":4,"./styles/_loading.js":5}],3:[function(require,module,exports){
 /**
  * header.js
  */
@@ -10121,6 +10122,104 @@ module.exports = function(Px2style){
 				"height": $modal.outerHeight() - $header.outerHeight() - $footer.outerHeight()
 			});
 		} catch (e) {}
+	}
+
+}
+
+},{"jquery":1}],5:[function(require,module,exports){
+/**
+ * loading.js
+ */
+module.exports = function(Px2style){
+	var $ = require('jquery');
+	var $loading,
+		$message,
+		$target;
+
+	/**
+	 * Open loading dialog.
+	 */
+	Px2style.prototype.loading = function(options, callback){
+		var _this = this;
+		callback = callback||function(){};
+
+		this.closeLoading(function(){
+			options = options||{};
+			options.target = options.target||$('body');
+
+			var tpl = '';
+			tpl += '<div>';
+			tpl += ' <div class="px2-loading"></div>';
+			tpl += '</div>';
+
+			$loading = $(tpl);
+			$message = $('<div>');
+			$message.css({
+				"clear": "both",
+				"font-size": "0.8rem",
+				"text-align": "center",
+				"color": "#999"
+			});
+
+			$target = $(options.target);
+			$target.append($loading);
+			$loading.append($message);
+
+			if( $target.get(0).tagName.toLowerCase() == 'body' ){
+				// body に挿入する場合は、 fixed に。
+				$loading.css({
+					"position": "fixed",
+					"left": 0,
+					"top": 0
+				});
+			}
+			$loading.css({
+				"width": '100%',
+				"height": '100%',
+				"display": "flex",
+				"justify-content": "center",
+				"flex-direction": "column",
+				"align-items": "center",
+				"z-index": 1000000
+			});
+
+			$(window).on('resize.px2-loading', function(){
+				onWindowResize();
+			});
+			onWindowResize();
+
+			callback();
+		});
+
+		return;
+	}
+
+	/**
+	 * Update loading message.
+	 */
+	Px2style.prototype.message = function(message){
+		$message.text(message);
+		return;
+	}
+
+	/**
+	 * Close loading dialog.
+	 */
+	Px2style.prototype.closeLoading = function(callback){
+		callback = callback||function(){};
+		try {
+			$loading.remove();
+		} catch (e) {}
+		$(window).off('resize.px2-loading');
+		callback();
+		return;
+	}
+
+	/**
+	 * Window Resize Event
+	 */
+	function onWindowResize(){
+		console.log('---- resize.px2-loading ----');
 	}
 
 }

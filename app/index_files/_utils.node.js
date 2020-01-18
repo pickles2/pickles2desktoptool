c95d6a4de6643728c886cc05b7c84c11d6a6a6d4
Tomoya Koyanagi <tomk79@gmail.com>
2020-01-18 20:25:01 +0900

    Linux版で、Git操作時にブランチ名の解析が失敗する場合がある問題を修正。

--- app/index_files/_utils.node.js
+++ app/index_files/_utils.node.js
@@ -36,7 +36,11 @@
 		// console.log( opts.cd );
 		// console.log( process.cwd() );
 
-		var proc = require('child_process').spawn(cmd, cliOpts);
+		var proc = require('child_process').spawn(cmd, cliOpts, {
+			env: {
+				LANG: 'en_US'
+			}
+		});
 		if( opts.success ){ proc.stdout.on('data', opts.success); }
 		if( opts.error ){ proc.stderr.on('data', opts.error); }
 		if( opts.complete ){ proc.on('close', opts.complete); }

d2380d8b444ed5aecd972e2ba3de7a9ceed00ae4
Tomoya Koyanagi <tomk79@gmail.com>
2020-02-28 10:18:02 +0900

    (update px2style)

--- app/common/px2style/dist/scripts.js
+++ app/common/px2style/dist/scripts.js
@@ -10026,6 +10026,7 @@ module.exports = function(Px2style){
 						_this.closeModal();
 					})
 			];
+			options.buttonsSecondary = options.buttonsSecondary||[];
 			options.target = options.target||$('body');
 			options.form = options.form||false;
 
@@ -10039,7 +10040,7 @@ module.exports = function(Px2style){
 			tpl += '      <div class="px2-modal__title"></div>';
 			tpl += '  </div>';
 			tpl += '  <div class="px2-modal__body"><div class="px2-modal__body-inner"></div></div>';
-			tpl += '  <div class="px2-modal__footer"></div>';
+			tpl += '  <div class="px2-modal__footer"><div class="px2-modal__footer-primary"></div><div class="px2-modal__footer-secondary"></div></div>';
 			tpl += ' </div>';
 			if(options.form){
 				tpl += '</form>';
@@ -10063,16 +10064,37 @@ module.exports = function(Px2style){
 			var $body = $modal.find('.px2-modal__body-inner');
 			$body.append( options.body );
 
-			var $footer = $modal.find('.px2-modal__footer');
+			function generateBtn(btnSetting){
+				btnSetting = btnSetting || {};
+				var $li = $('<li>');
+				var $btn = $(btnSetting);
+				$li.append($btn);
+				if( !$btn.attr('class') ){
+					$btn.attr({'class':'px2-btn'});
+				}
+				if( !$btn.text() ){
+					$btn.text('button');
+				}
+				return $li;
+			}
+
+			var $footer = $modal.find('.px2-modal__footer-primary');
 			var $footerUl = $('<ul>');
 			for( var i in options.buttons ){
-				var $li = $('<li>').append(options.buttons[i]);
-				$footerUl.append( $li );
+				$footerUl.append( generateBtn(options.buttons[i]) );
 			}
 			$footer.append($footerUl);
 
+			var $footer2 = $modal.find('.px2-modal__footer-secondary');
+			var $footer2Ul = $('<ul>');
+			for( var i in options.buttonsSecondary ){
+				$footer2Ul.append( generateBtn(options.buttonsSecondary[i]) );
+			}
+			$footer2.append($footer2Ul);
+
 			$target = $(options.target);
 			$target.append($modal);
+
 
 			if( $target.get(0).tagName.toLowerCase() == 'body' ){
 				// body に挿入する場合は、 fixed に。
@@ -10121,16 +10143,16 @@ module.exports = function(Px2style){
 	function onWindowResize(){
 		console.log('---- resize.px2-modal ----');
 		try {
-			if( $target.get(0).tagName.toLowerCase() != 'body' ){
-				$modal.css({
-					"height": $target.outerHeight()
-				});
-			}
-			var $header = $modal.find('.px2-modal__header');
-			var $footer = $modal.find('.px2-modal__footer');
-			$modal.find('.px2-modal__body').css({
-				"height": $modal.outerHeight() - $header.outerHeight() - $footer.outerHeight()
-			});
+			// if( $target.get(0).tagName.toLowerCase() != 'body' ){
+			// 	$modal.css({
+			// 		"height": $target.outerHeight()
+			// 	});
+			// }
+			// var $header = $modal.find('.px2-modal__header');
+			// var $footer = $modal.find('.px2-modal__footer');
+			// $modal.find('.px2-modal__body').css({
+			// 	"height": $modal.outerHeight() - $header.outerHeight() - $footer.outerHeight()
+			// });
 		} catch (e) {}
 	}
 

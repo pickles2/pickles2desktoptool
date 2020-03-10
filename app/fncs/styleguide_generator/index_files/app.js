77e236f5c9c60388c3ec534f0cee103db1e4dc95
Tomoya Koyanagi <tomk79@gmail.com>
2020-03-10 19:41:14 +0900

    styleguide生成機能の安定化。

--- app/fncs/styleguide_generator/index_files/app.js
+++ app/fncs/styleguide_generator/index_files/app.js
@@ -5,10 +5,12 @@ window.contApp = new (function( px ){
 	var _this = this;
 	var pj = this.pj = px.getCurrentProject();
 	var it79 = px.it79;
+	var utils79 = px.utils79;
 
 	var pxConf,
 		path_controot,
-		path_homedir;
+		path_homedir,
+		realpath_datadir;
 
 	var $content;
 	var broccoli,
@@ -21,9 +23,16 @@ window.contApp = new (function( px ){
 		it79.fnc({},
 			[
 				function(it1, arg){
+					pj.px2dthelperGetAll('/', {}, function(px2all){
+						realpath_datadir = px2all.realpath_homedir+'_sys/ram/data/';
+						it1.next(arg);
+					});
+					// console.log(arg);
+				},
+				function(it1, arg){
 					$content = $('.contents');
 					pxConf = pj.getConfig();
-					// console.log(pxConf);
+					console.log(pxConf);
 					pj.px2proj.get_path_homedir(function(_homedir){
 						console.log(_homedir);
 						path_homedir = _homedir;
@@ -33,16 +42,23 @@ window.contApp = new (function( px ){
 					});
 				},
 				function(it1, arg){
-					// broccoli を生成
+					// NodeJS版 broccoli を生成
+					if( pj.getGuiEngineName() != 'broccoli-html-editor' ){
+						it1.next(arg);
+						return;
+					}
 					pj.createBroccoliServer('/index.html', function(b){
 						broccoli = b;
-						// console.log(broccoli);
+						console.log(broccoli);
 						it1.next(arg);
 					});
 				},
 				function(it1, arg){
 					// broccoliStyleGuideGen
-					broccoliStyleGuideGen = new px.BroccoliStuleGuideGen(broccoli);
+					broccoliStyleGuideGen = new px.BroccoliStuleGuideGen({
+						"siteTitle": pxConf.name,
+						'gpiBridge': broccoliGpiBridge
+					});
 					// console.log(broccoliStyleGuideGen);
 					it1.next(arg);
 				},
@@ -64,23 +80,9 @@ window.contApp = new (function( px ){
 								} catch (e) {
 								}
 								try {
-									broccoliStyleGuideGen.generate(path_homedir+'styleguide/', {}, function(result){
-										// ↓broccoliStyleGuideGen では、SASSとnodeのバージョンが合わず
-										// SASSのコンパイルで異常終了する。
-										// そのための代替手段として、 px2dthelper にCSSのビルドを委託する。
-										pj.px2proj.query('/?PX=px2dthelper.document_modules.build_css', {
-											"output": "json",
-											"userAgent": "Mozilla/5.0",
-											"success": function(row){
-												// console.log(row);
-											},
-											"complete": function(bin, code){
-												// console.log(bin, code);
-												px.fs.writeFileSync( path_homedir+'styleguide/index_files/styles.css', bin );
-												px.message('スタイルガイドを生成しました。');
-												lockUIs(false);
-											}
-										});
+									broccoliStyleGuideGen.generate(path_homedir+'styleguide/', function(result){
+										px.message('スタイルガイドを生成しました。');
+										lockUIs(false);
 									});
 								} catch (e) {
 									console.error('ERROR: Failed to generate styleguide.', e);
@@ -122,6 +124,56 @@ window.contApp = new (function( px ){
 			$('.cont-open-dir').removeAttr('disabled');
 			px.progress.close();
 		}
+	}
+
+	function broccoliGpiBridge(api, options, callback){
+		// GPI(General Purpose Interface) Bridge
+		// broccoliは、バックグラウンドで様々なデータ通信を行います。
+		// GPIは、これらのデータ通信を行うための汎用的なAPIです。
+
+		if( pj.getGuiEngineName() != 'broccoli-html-editor' ){
+			// --------------------------------------
+			// PHP版 Broccoli を使用
+			var input = {
+				api: "broccoliBridge",
+				forBroccoli:{
+					api: api,
+					options: options
+				},
+				page_path: "/index.html"
+			};
+
+			// console.log('=-----=-----=', input);
+			// var testTimestamp = (new Date()).getTime();
+			var tmpFileName = '__tmp_'+utils79.md5( Date.now() )+'.json';
+			px.fs.writeFileSync( realpath_datadir+tmpFileName, JSON.stringify(input) );
+			pj.execPx2(
+				input.page_path+'?PX=px2dthelper.px2ce.gpi&appMode=desktop&target_mode=&data_filename='+encodeURIComponent( tmpFileName ),
+				{
+					complete: function(rtn){
+						// console.log('--- returned(millisec)', (new Date()).getTime() - testTimestamp);
+						try{
+							rtn = JSON.parse(rtn);
+						}catch(e){
+							console.error('Failed to parse JSON String -> ' + rtn);
+						}
+						px.fs.unlinkSync( realpath_datadir+tmpFileName );
+						// pj.updateGitStatus(function(){});
+						callback( rtn );
+					}
+				}
+			);
+			return;
+
+		}else{
+			// --------------------------------------
+			// NodeJS版 Broccoli を使用
+			broccoli.gpi(api, options, callback);
+			callback( false );
+			return;
+		}
+
+		return;
 	}
 
 	/**
